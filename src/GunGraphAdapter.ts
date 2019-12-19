dcb8b9c0c9adde5414ef037ae77bef8b9b21afa6
go1dfish <me@go1dfish.me>
2019-12-18 16:22:26 -0800

    Support for single key get option

--- src/GunGraphAdapter.ts
+++ src/GunGraphAdapter.ts
@@ -1,11 +1,15 @@
 import { GunGraphData, GunNode } from './Gun'
 
+export interface GunGetOpts {
+  readonly '.': string
+}
+
 export interface GunGraphAdapter {
   readonly close?: () => void
-  readonly get: (soul: string) => Promise<GunNode | null>
-  readonly getJsonString?: (soul: string) => Promise<string>
-  readonly getJsonStringSync?: (soul: string) => string
-  readonly getSync?: (soul: string) => GunNode | null
+  readonly get: (soul: string, opts?: GunGetOpts) => Promise<GunNode | null>
+  readonly getJsonString?: (soul: string, opts?: GunGetOpts) => Promise<string>
+  readonly getJsonStringSync?: (soul: string, opts?: GunGetOpts) => string
+  readonly getSync?: (soul: string, opts?: GunGetOpts) => GunNode | null
   readonly put: (graphData: GunGraphData) => Promise<GunGraphData | null>
   readonly putSync?: (graphData: GunGraphData) => GunGraphData | null
 }
